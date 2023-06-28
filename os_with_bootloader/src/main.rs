b3043cb16094eed34d19638a638ccfc86c7a2e10
georgeuwagbale <uwagbalegeorge@gmail.com>
2023-06-28 09:09:43 +0100

    Initial commit

--- os_with_bootloader/src/main.rs
+++ os_with_bootloader/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     // choose whether to start the UEFI or BIOS image
     // let uefi = true;
     let uefi = false;
-
+    
     let mut cmd = std::process::Command::new("qemu-system-x86_64");
     if uefi {
         cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
